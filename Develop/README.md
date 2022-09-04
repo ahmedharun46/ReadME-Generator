af06723f70c36a45d8bce783604ba2083e7a5fb3
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-09-04 00:14:22 -0500

    index js add part2

---
+++ Develop/README.md
@@ -0,0 +1,3 @@
+# Harun ReadMe
+  
+
--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [
     {
         type: 'input',
@@ -31,22 +33,43 @@ const questions = [
     {
         type: 'input',
         name: 'contribution',
-        message: 'How can users contribute to your project.?',
+        message: 'How can users contribute to your project?',
     },
     {
         type: 'input',
         name: 'test',
-        message: 'How can users contribute to your project.?',
+        message: 'Go the extra mile and write tests for your application.',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'userEmail',
+        message: 'What is your email address?',
     },
     
     
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+}
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() { }
+inquirer.prompt(questions).then((data) => {
+    //console.log(JSON.stringify(data, null, " "));//
+    writeToFile('README.md', data);
+});
+
 
 // Function call to initialize app
 init();
